c24b5f45e95a206fd11f7d536bc6364a0a117518
Nick the Sick <nicholas.perez@tiptap.dev>
2024-06-16 23:12:03 +0200

    chore: add db seeding

--- src/initialize.ts
+++ src/initialize.ts
@@ -3,3 +3,5 @@ const db = new Database("db/db.sqlite", { create: true });
 
 db.exec("PRAGMA journal_mode = WAL;");
 db.query("CREATE TABLE IF NOT EXISTS rsvps (name text, createdAt text);").run();
+
+db.close();
